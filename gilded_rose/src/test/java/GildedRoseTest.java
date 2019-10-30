e5b40b464701619710a8dc096cb576ac71ff5790
J <sousajf1@protonmail.ch>
2019-10-30 13:05:14 +0000

    [gilded_rose] [java] rename test method and make first approval test

--- gilded_rose/src/test/java/GildedRoseTest.java
+++ gilded_rose/src/test/java/GildedRoseTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 
 import gildedrose.GildedRose;
 import gildedrose.Item;
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,12 +11,13 @@ import java.util.*;
 public class GildedRoseTest {
 
 	@Test
-	public void testTheTruth() {
+	public void update_quality_approval_test() {
 		List<Item> items = new ArrayList<Item>();
 		items.add(new Item("newItem",0,0));
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		assertEquals("newItem", app.items.get(0).name);
+		//assertEquals("newItem", app.items.get(0).name);
+		Approvals.verify(app.items.get(0).name);
 	}
 
 }
