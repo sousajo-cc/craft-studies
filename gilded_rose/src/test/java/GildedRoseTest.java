9c0ddfe449d1e0527c0df3c8d524f09d716f25e2
J <sousajf1@protonmail.ch>
2019-10-30 13:49:10 +0000

    [gilded_rose] [java] test refactor to have combination approval

--- gilded_rose/src/test/java/GildedRoseTest.java
+++ gilded_rose/src/test/java/GildedRoseTest.java
@@ -1,8 +1,7 @@
-import static org.junit.Assert.*;
-
 import gildedrose.GildedRose;
 import gildedrose.Item;
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 import java.util.*;
@@ -10,14 +9,26 @@ import java.util.*;
 
 public class GildedRoseTest {
 
-	@Test
+	@Test 
 	public void update_quality_approval_test() {
-		List<Item> items = new ArrayList<Item>();
-		items.add(new Item("newItem",0,0));
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		//assertEquals("newItem", app.items.get(0).name);
-		Approvals.verify(app.items.get(0).name);
+		String name = "newItem";
+		int sellin = 0;
+		int quality = 0;
+		String itemString = doUpdate(name, sellin, quality);
+		//Approvals.verify(itemString);
+		CombinationApprovals.verifyAllCombinations(this::doUpdate,
+													new String[] {name},
+													new Integer[] {sellin},
+													new Integer[] {quality});
 	}
 
+	private String doUpdate(String name, int sellin, int quality) {
+		List<Item> items = new ArrayList<Item>();
+		items.add(new Item(name,sellin,quality));
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		return app.items.get(0).name + " , " +
+				app.items.get(0).sellIn + " , " +
+				app.items.get(0).quality;
+	}
 }
