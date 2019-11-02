d6c1f75311683dd7019be9ff202f2303423f25b7
J <sousajf1@protonmail.ch>
2019-11-02 22:36:42 +0000

    [gilded_rose] [scala] replaced quality - quality by 0

--- gilded_rose-scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ gilded_rose-scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -31,7 +31,7 @@ class GildedRose(val items: Array[Item]) {
           }
           item.sellIn = item.sellIn - 1
           if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality
+            item.quality = 0
           }
         case GildedRose.SULFURAS =>
         case _ =>
